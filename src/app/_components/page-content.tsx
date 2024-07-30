b22de95438f6166bc1f2edc0a8ce241e2ff13855
kleva-j <kasmickleva@gmail.com>
2024-07-30 16:45:24 +0100

    feat: add forms components and make minor changes

---
+++ src/app/_components/page-content.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { useState } from 'react';
+
+import { ApiRouteForm } from '@/components/api-route/form';
+import { FormActions } from '@/components/server-action/form';
+import { UploadThingForm } from '@/components/uploadthing/form';
+
+import { Card, CardDescription, CardHeader, CardTitle } from '@/ui/card';
+import { ScrollArea, ScrollBar } from '@/ui/scroll-area';
+import { Tabs, TabsList, TabsTrigger } from '@/ui/tabs';
+
+export const tabList = [
+  {
+    name: 'API Route Handler',
+    value: 'api-route',
+    tabTitle: 'With API Route',
+    description:
+      "Make changes to your API route here. Click save when you're done.",
+    component: ApiRouteForm,
+  },
+  {
+    name: 'With UploadThing',
+    value: 'upload-thing',
+    tabTitle: 'With UploadThing',
+    description:
+      "Make changes to your UploadThing here. Click save when you're done.",
+    component: UploadThingForm,
+  },
+  {
+    name: 'Using Server Action',
+    value: 'server-action',
+    tabTitle: 'With Server Actions',
+    description:
+      "Make changes to your Server Action here. Click save when you're done.",
+    component: FormActions,
+  },
+];
+
+export const tabListMap = tabList.reduce(
+  (acc, tab) => ({ ...acc, [tab.value]: tab }),
+  {}
+) as Record<string, (typeof tabList)[number]>;
+
+export const PageContent = () => {
+  const [activeTab, setActiveTab] = useState('api-route');
+  const tabContent = tabListMap[activeTab];
+  const CardContent = tabContent.component;
+
+  return (
+    <Tabs
+      defaultValue='api-route'
+      className='flex flex-col gap-y-2'
+      onValueChange={setActiveTab}
+    >
+      <ScrollArea>
+        <TabsList className='flex h-10 w-max mx-auto gap-x-2'>
+          {tabList.map((tab) => (
+            <TabsTrigger
+              key={tab.value}
+              value={tab.value}
+              className='flex items-center gap-x-2 px-4 py-2 text-sm font-medium text-gray-900 dark:text-gray-100 rounded-lg'
+            >
+              <span>{tab.name}</span>
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        <ScrollBar orientation='horizontal' />
+      </ScrollArea>
+      <Card className='w-full max-w-[500px] mx-auto'>
+        <CardHeader>
+          <CardTitle>{tabContent.tabTitle}</CardTitle>
+          <CardDescription>{tabContent.description}</CardDescription>
+        </CardHeader>
+        <div className='px-6 pb-6'>
+          <CardContent />
+        </div>
+      </Card>
+    </Tabs>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,10 @@
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { PageContent } from '@/app/_components/page-content';
 
 export default function HomePage() {
   return (
     <main className='flex-1'>
       <div className='max-w-xl px-4 mx-auto my-6 sm:my-12 md:my-16 lg:my-20'>
-        <Tabs defaultValue='api-route' className='flex flex-col gap-y-2'>
-          <ScrollArea className=''>
-            <TabsList className='flex h-10 w-max mx-auto'>
-              <TabsTrigger value='api-route'>With API Route</TabsTrigger>
-              <TabsTrigger value='upload-thing'>With UploadThing</TabsTrigger>
-              <TabsTrigger value='server-action'>
-                With Server Actions
-              </TabsTrigger>
-            </TabsList>
-            <ScrollBar orientation='horizontal' />
-          </ScrollArea>
-          <Card className='w-full max-w-[500px] mx-auto'>
-            <TabsContent value='api-route'>
-              <CardHeader>
-                <CardTitle>API Route</CardTitle>
-                <CardDescription>
-                  Make changes to your API route here. Click save when you're
-                  done.
-                </CardDescription>
-              </CardHeader>
-            </TabsContent>
-            <TabsContent value='upload-thing'>
-              <CardHeader>
-                <CardTitle>UploadThing</CardTitle>
-                <CardDescription>
-                  Make changes to your UploadThing here. Click save when you're
-                  done.
-                </CardDescription>
-              </CardHeader>
-            </TabsContent>
-            <TabsContent value='server-action'>
-              <CardHeader>
-                <CardTitle>Server Action</CardTitle>
-                <CardDescription>
-                  Make changes to your Server Action here. Click save when
-                  you're done.
-                </CardDescription>
-              </CardHeader>
-            </TabsContent>
-          </Card>
-        </Tabs>
+        <PageContent />
       </div>
     </main>
   );
